969c0afbc845fc6baf30d5432c4b9f1d239ad9cd
seperry83 <seperry83@gmail.com>
2022-08-30 11:57:26 -0700

    added station import func

--- 00_GlobalFunctions/functions.R
+++ 00_GlobalFunctions/functions.R
@@ -17,7 +17,38 @@ import_emp_data <- function(version = 'newest', show_version = FALSE, show_col_t
   download.file(url_emp, file.path(tempdir(), 'EMP_data.csv'), mode='wb')
   
   # import data
-  df_wq <- read_csv(file.path(tempdir(), 'EMP_data.csv'), show_col_types = show_col_types, col_types = col_types)
+  df_wq <- readr::read_csv(file.path(tempdir(), 'EMP_data.csv'), show_col_types = show_col_types, col_types = col_types)
   
   return(df_wq)
+}
+
+# import EMP data from EDI
+import_emp_stations <- function(version = 'newest', show_version = FALSE, show_col_types = FALSE, col_types = NULL, active_only = FALSE){
+  if (version != 'newest'){
+    url_emp <- paste0('https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.',as.character(version),'&entityid=ecf241d54a8335a49f8dfc8813d75609')
+  } else{
+    # url for most recent version
+    url_emp <- 'https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.7&entityid=ecf241d54a8335a49f8dfc8813d75609'
+  }
+  
+  # extract version number
+  if(show_version){
+    version_num <- str_remove(str_extract(url_emp, 'edi\\..*&'),'&')
+    print(paste('EMP data ID:',version_num))
+  }
+  
+  # download file
+  download.file(url_emp, file.path(tempdir(), 'EMP_stations.csv'), mode='wb')
+  
+  # import data
+  df_stations <- readr::read_csv(file.path(tempdir(), 'EMP_stations.csv'), show_col_types = show_col_types, col_types = col_types)
+  df_stations$StartDate <- as.Date(df_stations$StartDate, '%m/%d/%Y')
+  df_stations$EndDate <- as.Date(df_stations$EndDate, '%m/%d/%Y')
+  
+  # subset active
+  if (active_only){
+    df_stations <- df_stations[df_stations$Status == 'Active',]
+  }
+  
+  return(df_stations)
 }
