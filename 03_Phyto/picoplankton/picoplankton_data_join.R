87ae40d70f13061030abad971b5ddbbfb5162217
Flynn <Theodore.Flynn@water.ca.gov>
2022-09-14 17:49:27 -0700

    Add code for looking at AEU lab data and cosmetic fixes.

--- 03_Phyto/picoplankton/picoplankton_data_join.R
+++ 03_Phyto/picoplankton/picoplankton_data_join.R
@@ -16,7 +16,7 @@ rm(list=ls())
 # Define negate function
 `%!in%` = Negate(`%in%`)
 
-# Import list of sample dates and station IDs from BSA
+# Import list of sample dates and station IDs from BSA -------------------------
 df_pico <- read_csv("data/DSC_Picoplankton_Inventory_DWR.csv", col_select = 1:3)
 
 # Remove blank rows
@@ -53,7 +53,7 @@ df_pico <- df_pico %>%
                            StationCode == "SHR" ~ "AEU",
                            TRUE ~ "EMP"))
 
-## Read in AEU's field water quality data
+# Read in AEU's field water quality data ---------------------------------------
 df_AEU_WQ_Field <- read_csv("data/WQ_Data_AEU_Field.csv")
 
 # Get dates in the right format
@@ -67,7 +67,7 @@ df_AEU_WQ_Field$DateTime <- as_datetime(df_AEU_WQ_Field$DateTime,
                                 format = c("%Y-%m-%d %H:%M:%OS"))
 
 
-## Rename data frame headers to match EMP 
+# Rename data frame headers to match EMP 
 df_AEU_WQ_Field <- df_AEU_WQ_Field %>%
   rename("Secchi" = "secchi") %>%
   rename("WTSurface" = "water.temp") %>%
@@ -97,7 +97,7 @@ df_AEU_WQ_Field <- df_AEU_WQ_Field %>%
 # Correct the dates that don't mesh (assume times in BSA spreadsheet are incorrect)
 #sort(df_AEU_NAs$DateTime)
 
-## Import EMP WQ Data
+# Import EMP WQ Data -----------------------------------------------------------
 df_EMP_WQ <- read_csv("data/WQ_Data_EMP.csv")
 
 
@@ -106,22 +106,22 @@ df_EMP_WQ <- df_EMP_WQ %>%
   filter(Station %!in% c("LIS","STTD","SHR")) %>%
   rename("StationCode" = "Station")
 
-# Select only field data
+# Select only field water quality data
 df_EMP_WQ <- df_EMP_WQ %>%
   select(StationCode, Date, Time, Secchi, Microcystis, SpCndSurface, DOSurface, 
          WTSurface, TurbiditySurface_FNU, pHSurface)
 
-## Get dates in the right format
+# Get dates in the right format
 df_EMP_WQ$Date <- mdy(df_EMP_WQ$Date)
 
-## Combine date and time column
+# Combine date and time column
 df_EMP_WQ <- df_EMP_WQ %>% unite(DateTime, c("Date","Time"), sep = " ")
 
 df_EMP_WQ$DateTime <- as_datetime(df_EMP_WQ$DateTime, 
                                         tz = "US/Pacific",
                                         format = c("%Y-%m-%d %H:%M:%OS"))
 
-## Combined EMP and AEU data
+# Combined EMP and AEU data
 df_WQ_field <- bind_rows(df_AEU_WQ_Field,df_EMP_WQ)
 
 # Combine WQ data with list of samples from BSA
@@ -129,3 +129,25 @@ df_pico <- left_join(df_pico,df_WQ_field)
 
 # Export as CSV files
 write.csv(df_pico, file = "picoplankton_field_WQ.csv")
+
+# Import AEU WQ lab data -------------------------------------------------------
+
+# Read in raw data
+df_AEU_WQ_Lab <- read_csv("data/WQ_Data_AEU_Lab.csv")
+
+# Select non-duplicate samples
+df_AEU_WQ_Lab <- df_AEU_WQ_Lab %>% filter(SampleType == "Normal Sample")
+
+# Replace Bryte code with three-letter code
+df_AEU_WQ_Lab$StationNumber <- gsub("B9D82851352", "LIS", df_AEU_WQ_Lab$StationNumber)
+
+# Rename headers
+df_AEU_WQ_Lab <- df_AEU_WQ_Lab %>% 
+  rename("StationCode" = "StationNumber") %>%
+  rename("DateTime" = "CollectionDate")
+
+# Select analytes and dates
+df_AEU_WQ_Lab <- df_AEU_WQ_Lab %>% select(StationCode,DateTime:Analyte, Result)
+
+# Pivot wider
+test <- pivot_wider(df_AEU_WQ_Lab, names_from = Analyte, values_from = Result)
