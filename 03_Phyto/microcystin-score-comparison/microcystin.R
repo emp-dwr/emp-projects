8c8be96b7463b92b3acca2acb4f1678919a1a9a2
Flynn <Theodore.Flynn@water.ca.gov>
2022-10-27 18:11:50 -0700

    Update EMP phyto processing scripts; cosmetic changes

--- 03_Phyto/microcystin-score-comparison/microcystin.R
+++ 03_Phyto/microcystin-score-comparison/microcystin.R
@@ -16,60 +16,62 @@ theme_set(theme_bw())
 # Clean workspace
 rm(list=ls()) 
 
-## Read in data
-MC <- read_csv("San Francisco Estuary cyanoHAB data 2014 to 2019_v1.csv")
-WQ <- read_csv("SACSJ_delta_water_quality_1975_2020.csv")
-phyto <- read_csv("EMP_1975_2020_phyto.csv")
+# Read in EDI data from Tomo
+df_MC <- read_csv("data/San Francisco Estuary cyanoHAB data 2014 to 2019_v1.csv")
 
-unique(MC$EMP_Site)
-table(MC$EMP_Site)
-
-## Remove non-EMP sites
-MC <- MC %>%
+# Remove non-EMP sites
+df_MC <- df_MC %>%
   filter_at(vars(EMP_Site), all_vars(!is.na(.))) %>%
   filter(EMP_Site != "FieldCont") %>%
   filter(RepCont == 0)
 
-## Select only MC Score in WQ dataset
-WQ <- WQ %>% 
-  select(Station, Date, Microcystis) 
 
-## Remove unneeded cols in MC dataset
-MC$DWR_Site <- NULL
-MC$Lat <- NULL
-MC$Lon <- NULL
-MC$Collection_ID <- NULL
-MC[17:36] <- NULL
-MC$RepCont <- NULL
-MC$field.PST <- NULL
+
+# Remove unneeded cols in MC dataset
+df_MC <- df_MC %>%
+  select(EMP_Site, Collection_Date:Collection_Month,field.Water.temp:`ucd.elisa.ANA-a.dissolved.ugL`)
+
+# Clean up column names
+df_MC <- df_MC %>% clean_names(case = "snake")
 
 ## Rename columns to match
-MC <- MC %>%
-  rename(Date = Collection_Date) %>%
-  rename(Station = EMP_Site) %>%
-  rename(Year = Survey_Year) %>%
-  rename(Month = Collection_Month)
+df_MC <- df_MC %>%
+  rename(date = collection_date) %>%
+  rename(station = emp_site) %>%
+  rename(month = collection_month)
 
 ## Get dates to match
-MC$Date <- parse_date_time(MC$Date, 
+df_MC$date <- parse_date_time(df_MC$date, 
                       c("%Y-%m-%d"))
 
-WQ$Date <- parse_date_time(WQ$Date, 
+# Read in EDI from EMP Discrete Water Quality
+df_EMP_WQ <- read_csv("data/SACSJ_delta_water_quality_1975_2020.csv")
+
+# Clean up column names
+df_EMP_WQ <- df_EMP_WQ %>% clean_names(case = "snake")
+
+df_EMP_WQ$date <- parse_date_time(df_EMP_WQ$date, 
                            c("%m-%d-%Y"))
+
+# Select only MC Score in WQ dataset
+df_EMP_WQ <- df_EMP_WQ %>% select(station, date, microcystis) 
 
 ## EMP Phyto Data Massaging
 ## Import Phyto Data
 
+df_EMP_phyto <- read_csv("data/EMP_1975_2020_phyto.csv")
+
+
 ## Remove unneeded columns
-phyto[,c(2,4,5,8,9,10,12,13,14,15)] <- NULL
+df_EMP_phyto[,c(2,4,5,8,9,10,12,13,14,15)] <- NULL
 
 ## Rename date column
-phyto <- phyto %>%
+df_EMP_phyto <- df_EMP_phyto %>%
   rename(Date = SampleDate)
 
 ## Convert other dates and times into POSIXct-compliant values
 ## then remove old values
-phyto$Date <- parse_date_time(phyto$Date, 
+df_EMP_phyto$Date <- parse_date_time(df_EMP_phyto$Date, 
                            c("%m-%d-%Y"))
 
 ## Remove stations apart from modern ones
@@ -77,52 +79,52 @@ stations <- c("D22","NZ068","D19","D12","D16","D41A","D41","D28A","D26","NZ325",
               "NZS42","D10","EZ2","EZ6","D8","EZ2-SJR","NZ002","NZ004","NZ032",
               "D7","C9","C3A","MD10A","C10A","D4","P8","D6","EZ6-SJR")
 
-phyto <- phyto %>% filter(StationCode %in% stations)
+df_EMP_phyto <- df_EMP_phyto %>% filter(StationCode %in% stations)
 
 ## Rename Density & Station
-phyto <- phyto %>% 
+df_EMP_phyto <- df_EMP_phyto %>% 
   rename("Unit.Density" = "Organisms_per_mL") %>%
   rename("Station" = "StationCode")
 
 ## Summarize by genus
-phyto.gen <- phyto %>% 
+df_EMP_phyto_gen <- df_EMP_phyto %>% 
   group_by(Date, Station, Genus) %>%
   summarize(Unit.Density = sum(Unit.Density, na.rm = T)) %>%
   ungroup
 
 ## Filter out everything but Microcystis
-MC.EMP <- filter(phyto.gen, Genus == "Microcystis")
+df_EMP_MC <- filter(df_EMP_phyto_gen, Genus == "Microcystis")
 
 ## Rename Column Header and Remove Genus column
-MC.EMP <- MC.EMP %>% rename("EMP.MC.Unit.Density" = "Unit.Density")
-MC.EMP$Genus <- NULL
+df_EMP_MC <- df_EMP_MC %>% rename("EMP.MC.Unit.Density" = "Unit.Density")
+df_EMP_MC$Genus <- NULL
 
 ## Join datasets
-MC <- left_join(MC, WQ)
-MC <- left_join(MC, MC.EMP)
+df_MC <- left_join(df_MC, df_EMP_WQ)
+df_MC <- left_join(df_MC, df_EMP_MC)
 
 ## Replace NAs with zeros in EMP Unit Density values
-MC <- MC %>%
+df_MC <- df_MC %>%
   replace_na(list(EMP.MC.Unit.Density = 0))
 
 ## Rename category with Microcystis score
-MC <- MC %>%
+df_MC <- df_MC %>%
   rename(MC.Score = Microcystis)
 
 ## Remove half-score MC scores
-MC <- MC %>%
+df_MC <- df_MC %>%
   filter(MC.Score != 2.5) %>%
   filter(MC.Score != 3.5)
 
 ## Convert Microcystis scores to factors
-MC$MC.Score <- as.factor(MC$MC.Score)
+df_MC$MC.Score <- as.factor(df_MC$MC.Score)
 
 ## Filter out data pre-2015 (when MC SCore was implemented in EMP)
-MC <- MC %>%
+df_MC <- df_MC %>%
   filter(Year >= 2015)
 
 ## Plot Microcystis score vs. Microcystis biovolume
-plot1 <- ggplot(MC) +
+plot1 <- ggplot(df_MC) +
 #  geom_boxplot(aes(x = MC.Score,
 #                   y = log10(dwr.MIC_totbvL+1)),
 #               width = 0.1) +
@@ -147,7 +149,7 @@ ggsave(path="plots",
        dpi="print")
 
 ## Plot Microcystis score vs. EMP Microcystis Density
-plot2 <- ggplot(MC) +
+plot2 <- ggplot(df_MC) +
   geom_jitter(aes(x = MC.Score,
                   y = log10(EMP.MC.Unit.Density+1)),
               width = 0.1,
@@ -169,7 +171,7 @@ ggsave(path="plots",
        dpi="print")
 
 ## Plot MC Score vs. qPCR Microcystis
-plot3 <- ggplot(MC) +
+plot3 <- ggplot(df_MC) +
   geom_jitter(aes(x = MC.Score,
                    y = log10(ucd.ppia.MC.total.ugL+1)),
                width = 0.1)
@@ -177,14 +179,14 @@ plot3 <- ggplot(MC) +
 plot3
 
 ## Plot MC biovolume vs. qPCR Microcystis
-plot4 <- ggplot(MC) +
+plot4 <- ggplot(df_MC) +
   geom_point(aes(x = log10(dwr.MIC_totbvL+1),
                   y = log10(EMP.MC.Unit.Density+1)))
 
 plot4
 
 ## Plot EMP Unit Density vs Surface Tow
-plot5 <- ggplot(MC) +
+plot5 <- ggplot(df_MC) +
   geom_point(aes(x = log10(dwr.MIC_totbvL+1),
                   y = log10(EMP.MC.Unit.Density+1)),
               size = 2,
@@ -203,3 +205,11 @@ ggsave(path="plots",
        height=4,
        width=6, 
        dpi="print")
+
+# Compare chlorophyll in tows vs water grabs
+plot6 <- ggplot(df_MC) +
+  geom_point(aes(x = log10(dwr.MIC_totbvL+1),
+                 y = log10(EMP.MC.Unit.Density+1)),
+             size = 2,
+             color = "darkgreen")
+
