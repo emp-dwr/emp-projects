8899c8269756172e47b7131372a02a99e2f9af68
Flynn <Theodore.Flynn@water.ca.gov>
2023-01-03 10:17:51 -0800

    Initial commit.

---
+++ 03_Phyto/surface-phyto-study/surface-phyto-data-processing.R
@@ -0,0 +1,10 @@
+# Extracting phyto data from EMP datasheets
+# 10/25/2022
+
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+# Set working directory
+setwd("./03_Phyto/surface-phyto-study")
+getwd()
