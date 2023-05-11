74dcd2bc3fe9ae9002d96593f256df5e2efcf8b8
tedflynn <ted.flynn@gmail.com>
2023-05-10 17:52:53 -0700

    Initial commit for graphing C-EMP ratings data in 2022.

--- 04_Other/performance-metrics/EMP-performance-metrics-2022.R
+++ 04_Other/performance-metrics/EMP-performance-metrics-2022.R
@@ -14,6 +14,9 @@ getwd()
 # Clean workspace
 rm(list=ls()) 
 
+# Define negative %in% operator
+`%nin%` = Negate(`%in%`)
+
 # Set directory for storing plots
 output <- "plots"
 
@@ -66,3 +69,98 @@ ggsave(path = output,
        height=5,
        width=5, 
        dpi="print")
+
+# Read in C-EMP sonde data -----------------------------------------------------
+# Import data files 
+df_CEMP <- read_csv(file = "data/CEMP-sonde-ratings-2019-2022.csv")
+
+# Rename column headers
+df_CEMP <- df_CEMP %>% 
+  rename("Exchange_Date" = "Field Date") %>% 
+  rename("Rating" = "Wagner Rating")
+
+# Convert dates to date format
+df_CEMP$Exchange_Date <- mdy(df_CEMP$Exchange_Date)
+
+# Select only needed columns
+df_CEMP <- df_CEMP %>% 
+  select(Filename,Group,Parameter,Exchange_Date,Rating)
+
+# Filter to C-EMP Stations Only
+df_CEMP <- df_CEMP %>% 
+  filter(Group == "CEMP")
+
+# Pull StationID out of the filename using regular expressions and stringr
+df_CEMP <- df_CEMP %>% 
+  mutate(StationCode = str_extract(Filename, pattern = "(?<=DRIFT_)[^_]+"))
+
+# Remove unneeded stations and years
+df_CEMP <- df_CEMP %>% 
+  filter(StationCode %nin% c("GZB","2022","RRI A","RRI B","RRI C","RRI D")) %>%
+  filter(Exchange_Date >= "2022-01-01")
+
+df_CEMP_w <- pivot_wider(df_CEMP, names_from = Parameter, values_from = Rating)
+
+df_CEMP_w <- df_CEMP_w %>% 
+  group_by(StationCode) %>% 
+  mutate(Start_Date = lag(Exchange_Date)) %>%
+  ungroup()
+
+df_CEMP_w <- df_CEMP_w %>% 
+  mutate(Start_Date = case_when(is.na(Start_Date) ~ "2022-01-01",
+                          TRUE ~ as.character(Start_Date)))
+
+# Convert dates back to date format
+df_CEMP_w$Start_Date <- ymd(df_CEMP_w$Start_Date)
+
+# Relocate Start Date column
+df_CEMP_w <- df_CEMP_w %>% 
+  relocate(Start_Date, .before = Exchange_Date)
+
+# Calculate number of days between sonde exchanges
+df_CEMP_w <- df_CEMP_w %>%
+  mutate(Days_Rated = as.integer(Exchange_Date - Start_Date))
+
+# Summarize rating data for graphing -------------------------------------------
+df_CEMP_sum <- pivot_longer(df_CEMP_w, cols = DO:pH,
+                            values_to = "Rating",
+                            names_to = "Parameter")
+
+# Select needed data
+df_CEMP_sum <- df_CEMP_sum %>% 
+  select(StationCode:Rating)
+
+test <- df_CEMP_sum %>% 
+  group_by(StationCode, Parameter, Rating) %>% 
+  summarize(Total_Days = sum(Days_Rated)) %>% 
+  ungroup()
+
+# Plot C-EMP rating data -------------------------------------------------------
+colors = c("darkgreen","darkblue","yellow3","darkred")
+
+rating.plot <- ggplot(data = df_sondes, aes(x = PercentPass, 
+                                            y = Sensor, 
+                                            fill = Rating)) +
+  geom_bar(position = "stack",  
+           width = .6, 
+           stat = "summary", 
+           fun = "sum") +
+  scale_fill_manual(values = colors)
+
+rating.plot +
+  labs(x = "Rating of Data Collected (%)",
+       y = "Sensor",
+       fill = "Sonde Rating")
+
+ggsave(path = output,
+       filename = "DEMP_sonde_scores_2022.png", 
+       device = "png",
+       scale=1.0, 
+       units="in",
+       height=5,
+       width=5, 
+       dpi="print")
+
+
+
+
