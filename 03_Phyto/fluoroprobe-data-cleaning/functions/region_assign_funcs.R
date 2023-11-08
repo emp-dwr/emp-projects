1b45a11e975dcffa824972305fa8da2312461742
Sarah Perry <seperry83@gmail.comv>
2023-11-08 14:22:14 -0800

    update fluoro code

--- 03_Phyto/fluoroprobe-data-cleaning/functions/region_assign_funcs.R
+++ 03_Phyto/fluoroprobe-data-cleaning/functions/region_assign_funcs.R
@@ -1,19 +1,32 @@
 create_fl_dir <- function(year){
-  fp_emp <- 'California Department of Water Resources/Environmental Monitoring Program - Documents/Fluoroprobe'
-  dir_test <- paste0(Sys.getenv('USERPROFILE'),'/',fp_emp,'/Cleaned Data/',year)
+  fp_emp <- 'California Department of Water Resources/Environmental Monitoring Program - Documents/Water Quality/'
+  dir_test <- paste0(Sys.getenv('USERPROFILE'),'/',fp_emp)
   dir.create(dir_test, showWarnings = FALSE)
   return(NULL)
 }
 
+extract_year <- function(fp){
+  year <- str_extract(fp, '\\d{2}')
+  year <- paste0('20',year)
+  return(year)
+}
+
+extract_month <- function(fp){
+  month <- str_extract(fp, '\\d{8}')
+  month <- str_extract(month, '^..')
+  return(month)
+}
+
 abs_path <- function(fp_rel, type = NULL, year = NULL) {
-  fp_emp <- 'California Department of Water Resources/Environmental Monitoring Program - Documents/Fluoroprobe'
+  fp_emp <- 'California Department of Water Resources/Environmental Monitoring Program - Documents/'
+  
   
   if (type == 'fluoro') {
     fp_abs <-
       normalizePath(file.path(
         Sys.getenv('USERPROFILE'),
         fp_emp,
-        'Fluoroprobe Data/',
+        '00 - Monthly Run Docs/Raw Field Data/',
         fp_rel
       ))
     
