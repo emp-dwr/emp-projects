16959064f095e742e71f32eb8b0c653ca0df4aca
tedflynn <ted.flynn@gmail.com>
2024-09-05 12:30:53 -0700

    Add code to import EMP stations from CSV.

--- 04_Other/station-map-CCM-benthic-project/CCM-project-map.R
+++ 04_Other/station-map-CCM-benthic-project/CCM-project-map.R
@@ -22,3 +22,20 @@ theme_set(theme_bw())
 # Suppress summarise info
 options(tidyverse.quiet = TRUE)
 options(dplyr.summarise.inform = FALSE)
+
+# Load EMP station locations ---------------------------------------------------
+# Read in station locations for all compliance monitoring stations
+EMP_stations <- read_csv(here("04_Other",
+                              "station-map-CCM-benthic-project",
+                              "EMP-stations.csv"))
+
+# Convert to sf object
+sf_EMP <- st_as_sf(EMP_stations,
+                   coords = c("Longitude", "Latitude"), 
+                   crs = 4326)
+
+# Filter to stations used in CCM project
+sf_EMP <- sf_EMP %>% 
+  filter(StationID %in% c("D6-R","D7-C","D24-L","D16-L","P8-R"))
+
+# Create plot ------------------------------------------------------------------
